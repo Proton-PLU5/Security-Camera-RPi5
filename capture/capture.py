ef41ff36c0441051d6b65fce0dfce669f8c554bf
ProtonPLUS <cijinmathew33@gmail.com>
2026-07-10 22:11:48 +1200

    Add detection store to CaptureThread and update clip handling logic

--- capture/capture.py
+++ capture/capture.py
@@ -6,6 +6,7 @@ from picamera2.outputs import FfmpegOutput
 import logging
 import threading
 import os
+from capture.detection.detect import DetectionStore
 from capture.mailbox import MailBox
 from data.metrics import metrics
 from data.storage import StorageThread
@@ -18,6 +19,7 @@ class CaptureThread(threading.Thread):
                  detection_mailbox: MailBox,
                  stream_buffer: FrameBuffer,
                  storage_thread: StorageThread,
+                 detection_store: DetectionStore,
                  video_size: tuple[int, int] = (1920, 1080),
                  lowres_size: tuple[int, int] = (640, 640),
                  clip_dir: str = "clips",
@@ -31,6 +33,7 @@ class CaptureThread(threading.Thread):
         self.clip_dir = clip_dir
         self.clip_length = clip_length
         self.storage_thread = storage_thread
+        self.detection_store = detection_store
         self.camera = Picamera2()
         self.latest_clip_id = None
 
@@ -44,15 +47,17 @@ class CaptureThread(threading.Thread):
         self.encoder = H264Encoder(bitrate=10000000)
         self.stop_event = threading.Event()
         self.current_clip_start = 0.0
+        self.recording = False
 
     def run(self):
         self.camera.start()
-        self.latest_clip_id = self.start_new_clip()
+        self.current_clip_start = time.time() * 1000
 
         try:
             while not self.stop_event.is_set():
-                if self.clip_length and ((time.time() * 1000) - self.current_clip_start > (self.clip_length * 1000)):
-                    self.latest_clip_id = self.start_new_clip()
+                if (time.time() * 1000) - self.current_clip_start > (self.clip_length * 1000):
+                    self.current_clip_start = time.time() * 1000
+                    self.latest_clip_id = self.check_and_update_clip()
                 
                 request = self.camera.capture_request()
                 try:
@@ -78,9 +83,10 @@ class CaptureThread(threading.Thread):
         except Exception as e:
             logger.error(f"Error in CaptureThread: {e}")
         finally:
-            if self.latest_clip_id is not None:
+            if self.recording and self.latest_clip_id is not None:
                 end_time = time.time() * 1000
                 self.storage_thread.end_clip(self.latest_clip_id, ended_at=end_time)
+                self.camera.stop_encoder()
             self.camera.stop_encoder()
             self.camera.stop()
             self.camera.close()
@@ -89,21 +95,22 @@ class CaptureThread(threading.Thread):
     def stop(self):
         self.stop_event.set()
 
-    def start_new_clip(self) -> str:
+    def check_and_update_clip(self):
+        has_detection = len(self.detection_store.latest().boxes[0].boxes) > 0 # type: ignore
+
+        if self.recording and self.latest_clip_id is not None:
+            self.camera.stop_encoder()
+            self.storage_thread.end_clip(self.latest_clip_id, ended_at=time.time() * 1000)
+            self.recording = False
+
+        if not has_detection:
+            return None
+
         os.makedirs(self.clip_dir, exist_ok=True)
         filename = os.path.join(self.clip_dir, f"clip_{int(time.time())}.mp4")
         output = FfmpegOutput(filename)
-
-        if self.current_clip_start:
-            self.camera.stop_encoder()
-
-            if self.latest_clip_id is not None:
-                end_time = time.time() * 1000
-                self.storage_thread.end_clip(self.latest_clip_id, ended_at=end_time)
-
         self.camera.start_encoder(self.encoder, output)
+        self.recording = True
 
-        self.current_clip_start = time.time() * 1000
         logger.info(f"Started new clip: {filename}")
-
         return self.storage_thread.start_clip(filename, start_time=self.current_clip_start)
